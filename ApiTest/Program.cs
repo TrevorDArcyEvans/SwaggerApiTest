aa3e371539184c2bdb75d340f8db001252062674
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2021-04-11 16:26:07 +0100

    - initial version

---
+++ ApiTest/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace ApiTest
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      ImportFull();
+    }
+
+    private static void ImportFull()
+    {
+      var client = new RestClient("http://localhost:5000/");
+      var req = new RestRequest("/Importer/ImportFull", Method.POST);
+      var currExe = Assembly.GetExecutingAssembly().Location;
+      var data = new ImportFullSpecification
+      {
+        Links = new List<string>
+        {
+          "bbb",
+          "ccc"
+        },
+        ImportDefinitions = new List<ImportDefinition>
+        {
+          new ImportDefinition
+          {
+            Name = "xxx"
+          },
+          new ImportDefinition
+          {
+            Name = "yyy"
+          }
+        },
+        DataFile = currExe
+      };
+      var json = JsonConvert.SerializeObject(data);
+
+      // BEWARE - names have to match API parameter names!
+      req.AddHeader("importSpec", json);
+      req.AddFile("file", currExe);
+
+      var res = client.Execute<string>(req);
+      var apiRes = res.Data;
+
+      Console.WriteLine($"ImportFull --> {apiRes}");
+    }
+  }
+
+  #region Data Structures
+  public class ImportBaseSpecification
+  {
+    public List<string> Links { get; set; } = new List<string>();
+  }
+
+  public class ImportSpecification : ImportBaseSpecification
+  {
+    public string DataFile { get; set; }
+  }
+
+  public class ImportFullSpecification : ImportSpecification
+  {
+    public List<ImportDefinition> ImportDefinitions { get; set; } = new List<ImportDefinition>();
+  }
+
+  public class ImportDefinition
+  {
+    public string Name { get; set; }
+  }
+  #endregion
+}
---
+++ SwaggerTest/Controllers/ImporterController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SwaggerTest.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class ImporterController : ControllerBase
+  {
+    [HttpPost]
+    [Route(nameof(ImportFull))]
+    public string ImportFull(
+      [FromHeader] string importSpec,
+      IFormFile file)
+    {
+      var importSpecObj = JsonConvert.DeserializeObject<ImportFullSpecification>(importSpec);
+      var json = JsonConvert.SerializeObject(importSpecObj);
+      return file?.FileName + " --> " + json;
+    }
+  }
+
+  #region Data Structures
+  public class ImportBaseSpecification
+  {
+    public List<string> Links { get; set; } = new List<string>();
+  }
+
+  public class ImportSpecification : ImportBaseSpecification
+  {
+    public string DataFile { get; set; }
+  }
+
+  public class ImportFullSpecification : ImportSpecification
+  {
+    public List<ImportDefinition> ImportDefinitions { get; set; } = new List<ImportDefinition>();
+  }
+
+  public class ImportDefinition
+  {
+    public string Name { get; set; }
+  }
+  #endregion
+}
